043a01a851fdc9f50d566578c0ca054401e2c7b2
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2023-03-09 00:22:39 -0300

    create area restrict
    crração da area de login

--- pages/views/privateArea/index.js
+++ pages/views/privateArea/index.js
@@ -1,15 +1,5 @@
-import Head from 'next/head'
 import { useReducer, useState } from 'react'
-import { set } from 'mongoose'
-import Form from '@/components/Form/form'
-import Category from '@/components/Category/category'
-import InputText from '@/components/InputText/inputText'
 import styles from '@/styles/views/privateArea.module.css'
-import { MdHistory, MdArrowForwardIos, MdArticle } from 'react-icons/md';
-import { FcGoogle } from "react-icons/fc";
-import { AiFillFileText } from "react-icons/ai";
-import { HiOutlineMail, HiLockClosed } from "react-icons/hi";
-import { CgProfile} from "react-icons/cg";
 import Button from '@/components/Button/button'
 import Navbar from '@/components/Navbar'
 
@@ -34,14 +24,44 @@ export default function Home() {
     Senha:""
   });
 
-
   return (
     <div className={styles.mainContainer}>
-        <Navbar></Navbar>
-        <div className={styles.container}>
-              <div className={styles.barNavitaion} style={{backgroundColor:"red"}}>oi</div>
-              <div className={styles.areaData}>oi</div>
+      <Navbar></Navbar>
+      <div className={styles.container}>
+        <div className={styles.barNavitaion} >
+          <Button
+            shadow={"5px 5px 12px rgba(0,0,0,30%)"}
+            padding={"12px 19px"}
+            margintop={"1em"}
+            backgroundColor={'white'}
+            border={'none'}
+            cor={"black"}
+            width={90}>
+            Cadastrar
+          </Button>
+          <Button
+            shadow={"5px 5px 12px rgba(0,0,0,30%)"}
+            padding={"12px 19px"}
+            margintop={"1em"}
+            backgroundColor={'white'}
+            border={'none'}
+            cor={"black"}
+            width={90}>
+            Cadastrar
+          </Button>
+          <Button
+            shadow={"5px 5px 12px rgba(0,0,0,30%)"}
+            padding={"12px 19px"}
+            margintop={"1em"}
+            backgroundColor={'white'}
+            border={'none'}
+            cor={"black"}
+            width={90}>
+            Cadastrar
+          </Button>
         </div>
+        <div className={styles.areaData}>oi</div>
+      </div>
     </div>
 
   )
